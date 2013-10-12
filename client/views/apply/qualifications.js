206f64f979bc7e53da63636c2e09e8373e7ae57a
Tri Nguyen <tri@tridnguyen.com>
2013-10-12 17:30:06 -0400

    SAT scores validation, closes #14

--- client/views/apply/qualifications.js
+++ client/views/apply/qualifications.js
@@ -1,5 +1,24 @@
 Meteor.subscribe('languages');
 
+Template.qualifications.rendered = function() {
+	$("#qualifications").validate({
+		rules: {
+			reading: {
+				number: true,
+				range: [0, 800]
+			},
+			writing: {
+				number: true,
+				range: [0, 800]
+			},
+			math: {
+				number: true,
+				range: [0, 800]
+			}
+		}
+	})
+};
+
 Template.qualifications.languages = function() {
 	return Languages.find();
-}
+};
