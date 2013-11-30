fa4fc26a69889454842456313eab0c00b39af38b
Tri Nguyen <tri@tridnguyen.com>
2013-11-30 11:31:05 -0500

    save s3 url to database

--- client/helpers/readFile.js
+++ client/helpers/readFile.js
@@ -1,8 +1,15 @@
 readFile = function(file) {
-	var fileReader = new FileReader();
+	var fileReader = new FileReader(),
+		maxSize = 4194304; // 4MB
 
 	fileReader.onload = function() {
-		Meteor.call('saveFile', fileReader.result, file.name, file.type, file.size);
+		Meteor.call('saveFile', fileReader.result, file.name, file.type);
+	}
+
+	// @TODO check for file types again here
+	if (file.size >= maxSize) {
+		// @TODO add error handling for file that is too large
+		return;
 	}
 
 	fileReader.readAsBinaryString(file);
--- server/upload.js
+++ server/upload.js
@@ -7,29 +7,40 @@ var client = s3.createClient({
 	bucket: 'seo-vietnam'
 });
 
+var saveUrl = function(url, userId) {
+	console.log('saving ' + url + ' to user ' + userId);
+	Applications.update({'user': userId}, {
+		$set: {
+			'file-resume': url
+		}
+	});
+}
 
-var uploadFile = function(localFile) {
+var uploadFile = function(localFile, cb) {
 	var uploader = client.upload(localFile, 'resume-uploads/' + localFile);
 	uploader.on('error', function(err) {
-  console.error("unable to upload:", err.stack);
-});
-uploader.on('progress', function(amountDone, amountTotal) {
-  console.log("progress", amountDone, amountTotal);
-});
-uploader.on('end', function(url) {
-  console.log("file available at", url);
-});
+		console.error("unable to upload:", err.stack);
+		cb(new Meteor.Error(400, 'Unable to upload: ' + err.stack));
+	});
+	uploader.on('progress', function(amountDone, amountTotal) {
+		console.log("progress", amountDone, amountTotal);
+	});
+	uploader.on('end', function(url) {
+		console.log("file available at", url);
+	;	cb(null, url);
+	});
 // remove file after uploaded?
 }
 
-var saveFile = function(blob, name, type, size) {
-	fs.writeFile(name, blob, {
-		encoding: 'binary'
-	}, function() {
-		uploadFile(name);
-	});
+var saveFile = function(blob, name, type) {
+	var uploadFileSync = Meteor._wrapAsync(uploadFile);
+	fs.writeFileSync(name, blob, {encoding: 'binary'});
+	var s3Url = uploadFileSync(name);
+	if (s3Url) {
+		saveUrl(s3Url, this.userId);
+	}
 }
 
 Meteor.methods({
 	'saveFile': saveFile
-})
+});
