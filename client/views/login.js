1b566fd18cbb9b409d3d06845471d1090bebec57
Tri Nguyen <tri@tridnguyen.com>
2013-12-01 18:31:24 -0500

    simplify login screen

--- client/views/login.js
+++ client/views/login.js
@@ -4,79 +4,66 @@ Template.login.rendered = function() {
 	if (Meteor.user()) {
 		Meteor.Router.to('/apply');
 	}
+
+	$("#login").validate({
+		rules: {
+			email: {
+				email: true,
+				required: true
+
+			},
+			password: {
+				required: true,
+				minlength: 6
+			}
+		},
+		messages: {
+			password: {
+				minlength: "Your password needs to be at least 6 characters long"
+			}
+		}
+	});
 }
 
 Template.login.events = {
-	'click #start-button': function(e) {
+	'click #register-button': function(e) {
 		e.preventDefault();
-		var $form = $("#register");
-		$form.validate({
-			rules: {
-				email: {
-					email: true,
-					required: true
-
-				},
-				password: {
-					required: true,
-					minlength: 6
-				}
-			},
-			messages: {
-				password: {
-					minlength: "Your password needs to be at least 6 characters long"
-				}
-			}
-		});
-
-		if ($form.valid()) {
-			var email = $(".login-email", $form).val().trim(),
-				password = $(".login-password", $form).val().trim();
-			Accounts.createUser({
-				email: email,
-				password: password,
-				profile: {
-					roles: ['applicant']
-				}
-			}, function(err) {
-				if (err) {
-					$('.alert', $form).show().alert().find('.message').html(err.reason);
-					console.log(err);
-					return ;
-				}
-				newApplication();
-				Meteor.Router.to('/apply');
-			});
+		var $form = $('#login');
+		if (!$form.valid()) {
+			return;
 		}
+		var email = $(".login-email", $form).val().trim(),
+			password = $(".login-password", $form).val().trim();
+		Accounts.createUser({
+			email: email,
+			password: password,
+			profile: {
+				roles: ['applicant']
+			}
+		}, function(err) {
+			if (err) {
+				$('.alert', $form).show().alert().find('.message').html(err.reason);
+				return ;
+			}
+			newApplication();
+			Meteor.Router.to('/apply');
+		});
 	},
-	"click #login-button": function(e) {
+	"click #signin-button": function(e) {
 		e.preventDefault();
 		var $form = $("#login");
-		$form.validate({
-			rules: {
-				email: {
-					email: true,
-					required: true
-				},
-				password: {
-					required: true
-				}
-			}
-		});
-
-		if ($form.valid()) {
-			var email = $(".login-email", $form).val().trim(),
-				password = $(".login-password", $form).val().trim();
-			Meteor.loginWithPassword(email, password, function(err) {
-				if (err) {
-					console.log($('.alert', $form).find('.message'));
-					$('.alert', $form).show().alert().find('.message').html(err.reason);
-					console.log(err);
-					return ;
-				}
-				Meteor.Router.to('/apply');
-			});
+		if (!$form.valid()) {
+			return;
 		}
+		var email = $(".login-email", $form).val().trim(),
+			password = $(".login-password", $form).val().trim();
+		Meteor.loginWithPassword(email, password, function(err) {
+			if (err) {
+				$('.alert', $form).show().alert().find('.message').html(err.reason);
+				return ;
+			}
+			Meteor.Router.to('/apply');
+		});
 	},
 	"click .close": function(e) {
 		e.preventDefault();
