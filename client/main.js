29b2bf1dbf5827d68f692286222a5905ce81ce36
Tri Nguyen <tri@tridnguyen.com>
2013-12-12 11:51:01 -0500

    use url matching to identify production site

--- client/main.js
+++ client/main.js
@@ -57,15 +57,16 @@ Handlebars.registerHelper('isEmpty', function(thing, options) {
 });
 
 // Google Analytics
+var production_url = /^apply.seo-vietnam.org$/
 
-	if (process.env.APP_ENV && process.env.APP_ENV === 'production') {
+if (production_url.test(window.location.host)) {
 
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-26482928-4', 'seo-vietnam.org');
-		ga('send', 'pageview');
+	ga('create', 'UA-26482928-4', 'seo-vietnam.org');
+	ga('send', 'pageview');
 
-	}
+}
