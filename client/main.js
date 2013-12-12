f3594303e334ab00e0f3359d1c83634defa2c1d5
Tri Nguyen <tri@tridnguyen.com>
2013-12-12 11:15:00 -0500

    test GA for staging

--- client/main.js
+++ client/main.js
@@ -55,3 +55,18 @@ Handlebars.registerHelper('isEmpty', function(thing, options) {
 		return options.inverse(this);
 	}
 });
+
+// Google Analytics
+Template.main.created = function () {
+	if (process.env.APP_ENV && process.env.APP_ENV === 'staging') {
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-26482928-4', 'seo-vietnam.org');
+		ga('send', 'pageview');
+
+	}
+}
