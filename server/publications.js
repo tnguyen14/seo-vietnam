337249bbed1509be88bc4db73682b48ef2c82c40
Tri Nguyen <tri@tridnguyen.com>
2013-12-26 22:58:53 -0500

    add roles to client user data

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,12 @@ Meteor.publish('information', function(){
 Meteor.publish('app', function(){
 	return Applications.find({user: this.userId});
 });
+
+Meteor.publish("userData", function () {
+	return Meteor.users.find({_id: this.userId},
+	{
+		fields: {
+			'roles': 1
+		}
+	});
+});
