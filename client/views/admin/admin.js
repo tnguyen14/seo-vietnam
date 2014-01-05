65ae7f40125c5f2b2a26bbeee54fdcc11cd78d33
Tri Nguyen <tri@tridnguyen.com>
2014-01-05 17:05:19 -0500

    modifield stats on completed apps

--- client/views/admin/admin.js
+++ client/views/admin/admin.js
@@ -21,3 +21,28 @@ AdminController = RouteController.extend({
 		}
 	}
 });
+
+function appReady(app) {
+	var empty = [],
+		required = ['essay.one', 'essay.two', 'essay.three', 'essay.four', 'files.resume'];
+	_.each(required, function(field){
+		// parse the dot notation
+		var value = app,
+			path = field.split('.');
+		while (path.length !== 0) {
+			if (!value) {
+				break;
+			}
+			value = value[path.shift()];
+		}
+		if (_.isEmpty(value)) {
+			empty.push(field);
+		}
+	});
+
+	if (empty.length > 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
