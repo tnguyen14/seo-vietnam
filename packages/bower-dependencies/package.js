38eeabc8b22e1fcdf7dd1de69e25fc966c0106b8
Tri Nguyen <tri@tridnguyen.com>
2014-01-06 01:02:07 -0500

    use font-awesome

--- packages/bower-dependencies/package.js
+++ packages/bower-dependencies/package.js
@@ -5,9 +5,11 @@ Package.describe({
 Package.on_use(function(api) {
 	// simply countable
 	api.add_files(['../../public/bower_components/jquery-simply-countable/jquery.simplyCountable.js'], 'client');
+
 	// jquery validate
 	api.add_files(['../../public/bower_components/jquery.validation/jquery.validate.js'], 'client');
 	api.add_files(['../../public/bower_components/jquery.validation/additional-methods.js'], 'client');
+
 	// bootstrap
 	api.add_files(['../../public/bower_components/bootstrap/dist/js/bootstrap.min.js'], 'client');
 	api.add_files([
@@ -16,12 +18,24 @@ Package.on_use(function(api) {
 		'../../public/bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
 		'../../public/bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff'
 	], 'client');
+
+	// font-awesome
+	api.add_files([
+		'../../public/bower_components/font-awesome/css/font-awesome.css',
+		'../../public/bower_components/font-awesome/fonts/fontawesome-webfont.eot',
+		'../../public/bower_components/font-awesome/fonts/fontawesome-webfont.svg',
+		'../../public/bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
+		'../../public/bower_components/font-awesome/fonts/fontawesome-webfont.woff',
+		'../../public/bower_components/font-awesome/fonts/FontAwesome.otf'
+	], 'client')
+
 	// ladda-bootstrap
 	api.add_files([
 		'../../public/bower_components/ladda-bootstrap/dist/ladda-themeless.min.css',
 		'../../public/bower_components/ladda-bootstrap/dist/spin.min.js',
 		'../../public/bower_components/ladda-bootstrap/dist/ladda.min.js'
 	], 'client');
+
 	// jquery cookie
 	api.add_files([
 		'../../public/bower_components/jquery.cookie/jquery.cookie.js'
