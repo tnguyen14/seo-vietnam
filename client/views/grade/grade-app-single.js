531faf58e2685db917a672d4ef0eb4dd1dc64e79
Tri Nguyen <tri@tridnguyen.com>
2014-01-15 01:27:28 -0500

    update grader status

--- client/views/grade/grade-app-single.js
+++ client/views/grade/grade-app-single.js
@@ -7,7 +7,8 @@ GradeAppSingleController = RouteController.extend({
 		// @TODO logic to improve privary and only load assigned app
 		return [
 			Meteor.subscribe('graderApps'),
-			Meteor.subscribe('graderUsers')
+			Meteor.subscribe('graderUsers'),
+			Meteor.subscribe('graderData', Meteor.userId())
 		]
 	},
 	data: function() {
@@ -68,10 +69,13 @@ Template['grade'].events = {
 		e.preventDefault();
 		var g = {},
 			appId = this.app._id,
+			applicantId = this.app.user,
 			graderId = Meteor.userId(),
 			// grades is an array
 			// [{"criterion": "score"}, {"criterion2": "score2"}]
-			grades= getFormGroups($(e.target).closest('.criteria'));
+			grades = getFormGroups($(e.target).closest('.criteria')),
+			saveGradeDfd = new $.Deferred(),
+			saveGraderStatusDfd = new $.Deferred();
 		// combine all the grades together into a single object
 		_.each(grades, function(grade) {
 			_.each(grade, function(score,slug) {
@@ -79,6 +83,7 @@ Template['grade'].events = {
 				g[slug] = score;
 			});
 		});
+		// add grader id
 		g.grader = graderId;
 		// add g to the grades array
 		Applications.update(appId, {
@@ -93,14 +98,53 @@ Template['grade'].events = {
 					$push: {grades: g}
 				}, function(err) {
 					if (!err) {
-						notify({
-							message: "Grades saved",
-							context: "success",
-							auto: true
-						});
+						saveGradeDfd.resolve();
+					} else {
+						saveGradeDfd.reject(err);
 					}
 				});
+			} else {
+				saveGradeDfd.reject(err);
 			}
 		});
+		Meteor.users.update(graderId, {
+			$pull: {
+				'grader.apps': {appId: appId}
+			}
+		}, function(err) {
+			if (!err) {
+				Meteor.users.update(graderId, {
+					$push: {
+						'grader.apps': {
+							appId: appId,
+							applicantId: applicantId,
+							status: 'graded'
+						}
+					}
+				}, function(err) {
+					if (!err) {
+						saveGraderStatusDfd.resolve();
+					} else {
+						saveGraderStatusDfd.reject(err);
+					}
+				})
+			} else {
+				saveGraderStatusDfd.reject(err);
+			}
+		});
+		$.when(saveGradeDfd.promise(), saveGraderStatusDfd.promise())
+		.done(function() {
+			notify({
+				message: "Grades saved",
+				context: "success",
+				auto: true
+			});
+		}).fail(function(err) {
+			notify({
+				message: err.reason,
+				context: "danger",
+				dismissable: true
+			});
+		})
 	}
 }
