ed812ccc4c19a5f7e16b4dc92778b8f21316a214
Tri Nguyen <tri@tridnguyen.com>
2014-03-18 02:05:38 -0400

    first step at generating report

--- client/views/admin/admin.js
+++ client/views/admin/admin.js
@@ -1,3 +1,5 @@
+var totalApps, submittedApps, completedApps;
+
 AdminController = RouteController.extend({
   waitOn: function() {
     return [
@@ -8,13 +10,13 @@ AdminController = RouteController.extend({
   data: function() {
     Meteor.subscribe('allApps'),
     Meteor.subscribe('allUsers')
-    var totalApps = Applications.find().fetch(),
-      submittedApps = Lazy(totalApps).filter(function(app) {
-        return app.status === 'completed';
-      }),
-      completedApps = Lazy(totalApps).filter(function(app) {
-        return appComplete(app);
-      });
+    totalApps = Applications.find().fetch();
+    submittedApps = Lazy(totalApps).filter(function(app) {
+      return app.status === 'completed';
+    });
+    completedApps = Lazy(totalApps).filter(function(app) {
+      return appComplete(app);
+    });
     return {
       totalApps: totalApps.length,
       submittedApps: submittedApps.size(),
@@ -48,6 +50,78 @@ appComplete = function (app) {
     return true;
   }
 }
+
+var getName = function(name) {
+  return name.first + ' ' + name.middle + ' ' + name.last;
+}
+
+Template['admin'].events = {
+  'click #export-data': function() {
+    var header = 'userId,name,email,phone,school,location,graduationYear,appId,grader1,grade1,grade1Comment,grader2,grade2,grade2Comment,grader3,grade3,grade3Comment,interviewer,interviewScore,interviewComment',
+      body = '';
+    completedApps.each(function(app) {
+      // userId
+      body += app.user + ',';
+      var user = Meteor.users.findOne(app.user),
+        grade1 = grade2 = grade3 = interview = new Array(3),
+        grader1, grader2, grader3, interviewer;
+      // name
+      body += getName(user.profile.name);
+      // email
+      body += user.emails[0].address + ',';
+      // phone
+      body += user.profile.phone + ',';
+      // school
+      body += app.college + ',';
+      // location
+      body += user.profile['country-residence'] + ',';
+      // graduationYear
+      body += app['graduation-date'] + ',';
+      // appId
+      body += app._id + ',';
+      // grader1
+      if (app.graders && app.graders.length > 0) {
+        grader1 = Meteor.users.findOne(app.graders[0]);
+        if (grader1) {
+          grade1[0] = getName(grader1.profile.name);
+        }
+      }
+      body += grade1.join(',') + ',';
+      // grader2
+      if (app.graders && app.graders.length > 1) {
+        grader2 = Meteor.users.findOne(app.graders[1]);
+        if (grader2) {
+          grade2[0] = getName(grader2.profile.name);
+        }
+      }
+      body += grade2.join(',') + ',';
+      // add grader 3 if there's difference of more than 2
+      if (grade1[1] && grade2[1] && Math.abs(grade1[1] - grade2[1]) >= 2) {
+        if (app.graders.length > 2) {
+          grader3 = Meteor.users.findOne(app.graders[2]);
+          if (grader3) {
+            grade3[0] = getName(grader3.profile.name);
+          }
+        }
+      }
+      body += grade3.join(',') + ',';
+      // interviewer
+      if (app.interviewers && app.interviewers.length > 0) {
+        interviewer = Meteor.users.findOne(app.interviewers[0]);
+        if (interviewer) {
+          interview[0] = getName(interviewer.profile.name);
+        }
+      }
+      body += interview.join(',') + ',';
+      body += '\r\n';
+    });
+    var csv = new Blob([header, body], {type: 'text/csv'}),
+        a = document.createElement('a');
+    a.href = window.URL.createObjectURL(csv);
+    a.innerHTML = 'Download';
+    document.getElementsByClassName('admin-content')[0].appendChild(a);
+  }
+};
 
 Template['admin-stats'].events = {
   'click #assign-apps': function() {
